a6ee6e8ed831453ea8302e9a44e3852d11d38f8c
sachin-wsc <sachin.prajapati@weservecodes.com>
2026-03-10 17:13:01 +0530

    env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "drizzle-kit";
+// import { defineConfig } from "drizzle-kit";
 
-export default defineConfig({
-    schema: "./src/db/schema.ts",
-    out: "./src/db/migrations",
-    dialect: "mysql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL || "",
-    },
-    verbose: true,
-    strict: true,
+// export default defineConfig({
+//     schema: "./src/db/schema.ts",
+//     out: "./src/db/migrations",
+//     dialect: "mysql",
+//     dbCredentials: {
+//         url: process.env.DATABASE_URL || "",
+//     },
+//     verbose: true,
+//     strict: true,
+// });
+
+
+import mysql from "mysql2/promise";
+import { drizzle } from "drizzle-orm/mysql2";
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "",
+    port: 3306
 });
+
+export const db = drizzle(pool);
