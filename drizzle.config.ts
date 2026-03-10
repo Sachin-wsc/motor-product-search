85853219f969fc5fe12c91c31e98d895c0f98687
sachin-wsc <sachin.prajapati@weservecodes.com>
2026-03-10 17:03:01 +0530

    - env change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
-// drizzle.config.ts
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: './src/db/schema.ts',
-    out: './src/db/migrations',
-    dialect: 'mysql',
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
+    dialect: "mysql",
     dbCredentials: {
-        host: 'localhost',
-        user: process.env.DB_USER || "",
-        password: process.env.DB_PASSWORD || "",
-        database: process.env.DB_NAME || "",
-        port: 3306,
+        url: process.env.DATABASE_URL || "",
     },
     verbose: true,
     strict: true,
