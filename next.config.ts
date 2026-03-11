35e1be5f2d82934a11f93bb67a6f375975717b3e
sachin-wsc <sachin.prajapati@weservecodes.com>
2026-03-11 11:00:51 +0530

    - image path configuration in next config file

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ],
       },
+      {
+        // Match all uploaded static files
+        source: "/uploads/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type" },
+        ],
+      },
     ];
   },
 };
